51c741fc6a5746eb8117472564e334b78a23005f
Micah J. Martin <mjm5097@rit.edu>
2018-11-28 19:48:31 -0500

    Fixed randomizer

--- assets/js/sponsors.js
+++ assets/js/sponsors.js
@@ -2,16 +2,28 @@
 ---
 //sponsors.js
 // Handles the swapping of sponsor images on the homepage
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
 $(document).ready(function(){
     var images = [
-        {% for sponsor in site.data.sponsors %}['assets/images/sponsorLogos/{{ sponsor.image }}', "{{ sponsor.name }}"],{% endfor %}
+        {% for sponsor in site.data.sponsors %}['assets/images/sponsorLogos/{{ sponsor.image }}', "{{ sponsor.link }}"],{% endfor %}
     ];
-
-    for(i=0;i<images.length;i++){
-        var index = Math.floor(Math.random()*images.length)
-        var item = images[index];
+    images = shuffle(images);
+    for(i=0;i<4;i++){
+        var item = images[i];
         $('#sponsor'+i).attr("src", item[0]);
         $('#sponsor'+i).parent("a").attr("href", item[1]);
-        images.splice(index,1);
     }
 });
+
